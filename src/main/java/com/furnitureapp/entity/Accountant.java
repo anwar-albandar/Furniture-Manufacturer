ef1eadbcc5b23e089a757f84661d3ca8b607f152
elvis-gene <elvis.pgene@gmail.com>
2020-06-29 02:56:38 +0200

    Accounting Sub-system Entities Coded

--- src/main/java/com/furnitureapp/entity/Accountant.java
+++ src/main/java/com/furnitureapp/entity/Accountant.java
@@ -8,12 +8,23 @@ public class Accountant extends Employee{
 
     @Override
     public String toString() {
-        return  super.toString();
+        return super.toString();
     }
 
     public static class AccountantBuilder extends EmployeeBuilder{
 
         public AccountantBuilder(){}
+
+        public AccountantBuilder copy(Accountant accountant){
+            super.setEmployeeCode(accountant.getEmployeeCode());
+            super.setFullName(accountant.getFullName());
+            super.setAddress(accountant.getAddress());
+            super.setEmailAddress(accountant.getEmailAddress());
+            super.setPhone(accountant.getPhone());
+            super.setJob(accountant.getJob());
+            super.setPassCode(accountant.getPassCode());
+            return this;
+        }
 
         public Accountant build(){
             return new Accountant(this);
